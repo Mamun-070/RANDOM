f8f10ae7c78b6b7e5e4dfbdf49f22ae7cead68ac
Mamun-070 <118709207+Mamun-070@users.noreply.github.com>
2022-12-03 17:24:29 +0600

    Create MAINTANANCE.py

---
+++ MAINTANANCE.py
@@ -0,0 +1,16 @@
+try:
+	import concurrent.futures
+except ImportError:
+	os.system("pip install futures")
+	runtxt("\n\033[0;91m This tool is Under maintenance break ")
+	runtxt("\n\033[0;91m So wait For Update ")
+print('[+] This tool is Under maintenance break FROM 3/12/22 TO /10/12/22 .......')
+print('[+]    So wait For Update .......')
+import os
+import sys
+import time
+import requests
+import random
+import platform
+import base64
+import subprocess
