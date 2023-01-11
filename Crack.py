49de4f579bc4dda8b41903997938866ef69094ea
Mamun-070 <118709207+Mamun-070@users.noreply.github.com>
2023-01-12 00:11:03 +0600

    Update Crack.py

--- Crack.py
+++ Crack.py
@@ -6,5 +6,14 @@ bit = platform.architecture()[0]
 if bit=='64bit':
     print('\033[1;32m[•] Congrats! Your Device Support This Tools')
     os.system('xdg-open https://www.facebook.com/groups/1087082191981905/?ref=share_group_link')
+import os,platform
+os.system('clear')
+print('[•] Checking Updates...')
+os.system('git pull')
+bit = platform.architecture()[0]
+if bit=='64bit':
+    print('\033[1;32m[+] PLIS FOLLOW MY GITHUB')
+    os.system('xdg-open https://github.com/MR-OV1')
+
 import Sex
 Sex.menu()
