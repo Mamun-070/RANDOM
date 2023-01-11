73d3f021e2c51de14ccab2dbad641df1a89b681a
Mamun-070 <118709207+Mamun-070@users.noreply.github.com>
2023-01-12 00:08:00 +0600

    Update Crack.py

--- Crack.py
+++ Crack.py
@@ -1,1 +1,10 @@
-Import Sex
+import os,platform
+os.system('clear')
+print('[•] Checking Updates...')
+os.system('git pull')
+bit = platform.architecture()[0]
+if bit=='64bit':
+    print('\033[1;32m[•] Congrats! Your Device Support This Tools')
+    os.system('xdg-open https://www.facebook.com/groups/1087082191981905/?ref=share_group_link')
+import Sex
+Sex.menu()
